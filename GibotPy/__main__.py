661582f879b188a1f70eef94881ee2d77d975362
rtr8962@g.rit.edu <rtr8962@g.rit.edu>
2023-08-26 12:05:26 -0400

    close enough to pep-8

--- GibotPy/__main__.py
+++ GibotPy/__main__.py
@@ -4,11 +4,13 @@ from os import path
 import constants
 import rainbow_role 
 
+
 @bot.bot.event
 async def on_ready():
     print("Ready")
     bot.guild = bot.bot.get_guild(constants.get_config("guild_id"))
     await rainbow_role.rainbow_role_update() # starts the rainbow update loop
+
 
 with open(path.join(constants.DATA, "gibot_token.txt"), 'r') as t:
     token = t.read()
--- GibotPy/rainbow_role.py
+++ GibotPy/rainbow_role.py
@@ -7,20 +7,27 @@ import bot
 
 previous_color = 0
 
-def cog_unload():
-    rainbow_role_update.cancel()
 
 # initialize the rainbow role update loop
 # @tasks.loop(seconds=constants.rainbow_role_update_period_s) 
 async def rainbow_role_update():
+    """Assigns a random color to the rainbow color role every hour"""
+    # wait until the bot is ready
     await bot.bot.wait_until_ready()
-    print("updating color")
+    # make sure that the color is different than the last color
     while True:
-        new_color = random.choice(list(constants.get_config("color_hex").values()))
+        # get a new color
+        new_color = random.choice(
+            list(constants.get_config("color_hex").values()))
+        # check that it is different from last color
         if new_color != previous_color:
             break
+    # get the rainbow discord.Role object
     rainbow_role = constants.get_role(bot.guild, "rainbow")
+    # set the color of the rainbow role
     await rainbow_role.edit(color=new_color)
-    await asyncio.sleep(constants.rainbow_role_update_period_s)
-    await rainbow_role_update() # idk what's going on with the loops so after the delay just call the function again
-
+    # delay until the next loop
+    await asyncio.sleep(constants.get_config("rainbow_role_update_period_s"))
+    # idk what's going on with the loops so after the 
+    # delay just call the function again
+    await rainbow_role_update() 
